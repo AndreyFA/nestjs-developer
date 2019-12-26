4fbcd0ca42c4df770ae218634dcf24529e42f493
Andrey Frazatti Alves <andrey.alves@tokstok.com.br>
2019-12-26 19:33:26 -0300

    Projeto

---
+++ src/backoffice/controllers/customer.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Put, Delete, Param, Body, UseInterceptors } from '@nestjs/common';
+import { Customer } from '../models/customer.model';
+import { Result } from '../models/result.model';
+import { ValidatorInterceptor } from 'src/interceptors/validator.interceptor';
+import { CreateCustomerContract } from '../contracts/customer.contracts';
+
+@Controller('v1/customers')
+export class CustomerController {
+    @Get()
+    get(): Result {
+        return new Result(null, true, [], null);
+    }
+
+    @Get(':document')
+    getById(@Param('document') document: string): Result {
+        return new Result(null, true, {}, null);
+    }
+
+    @Post()
+    @UseInterceptors(new ValidatorInterceptor(new CreateCustomerContract()))
+    post(@Body() customer: Customer): Result {
+        return new Result('Cliente cadastrado com sucesso!', true, customer, null);
+    }
+
+    @Put(':document')
+    put(@Param('document') document: string, @Body() customer: Customer): Result {
+        return new Result('Cliente atualizado com sucesso!', true, customer, null);
+    }
+
+    @Delete(':document')
+    delete(@Param() document: string): Result {
+        return new Result('Cliente removido com sucesso!', true, null, null);
+    }
+}
---
+++ src/backoffice/models/credit-card.model.ts
@@ -0,0 +1,6 @@
+export class CreditCard {
+    constructor(
+        public holder: string,
+        public cardNumber: string,
+        public expiration: string) { }
+}
